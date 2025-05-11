aa8657cf89f527bad2c2ff69f790957d4de4874b
ms23107 <152854323+ms23107@users.noreply.github.com>
2025-05-11 13:22:01 +0300

    Add files via upload

---
+++ Services/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BookStore.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var fromEmail = "email";
+            var fromPassword = @"password";
+            using MailMessage message = new();
+            message.From = new MailAddress(fromEmail);
+            message.To.Add(new MailAddress(email));
+            message.Subject = subject;
+            message.Body = htmlMessage;
+            message.IsBodyHtml = true;
+            using SmtpClient smtpClient = new("smtp.mailersend.net")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromEmail, fromPassword),
+                EnableSsl = true
+            };
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
